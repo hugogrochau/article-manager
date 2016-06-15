153f5bd3120e9432f18d10f7673b67e701ed2b10
hugogrochau <hugo@grochau.com>
2016-06-14 23:03:33 -0300

    Add test insert person route

--- app/app.lua
+++ app/app.lua
@@ -1,11 +1,21 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+local Person = require("models.person")
+local db = require("lapis.db")
 
 app:enable("etlua")
-app.layout = require "views.layout"
+app.layout = require("views.layout")
 
 app:get("/", function(self)
     return { render = "index" }
+end)
+
+app:match("/person", function(self)
+    person = Person:create({
+        email = self.params.email,
+        name = self.params.name
+    })
+    return "Created person called " .. person.name .. " with the email " .. person.email
 end)
 
 return app
