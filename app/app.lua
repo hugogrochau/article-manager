fd1b27e2a405487b02cc73c669c6de3e9cf1c78e
mpolitzer <mpolitzer.c@gmail.com>
2016-06-20 22:51:01 -0300

    Added sample articles viewer page

--- app/app.lua
+++ app/app.lua
@@ -1,13 +1,33 @@
 local lapis = require("lapis")
 local app = lapis.Application()
 local Person = require("models.person")
-local db = require("lapis.db")
+--local db = require("lapis.db")
 
 app:enable("etlua")
 app.layout = require("views.layout")
 
 app:get("/", function(self)
     return { render = "index" }
+end)
+
+
+app:match("/articles", function(self)
+  local articles= {}
+
+  articles[1] = {}
+  articles[1].title = "article_a"
+  articles[1].desc = "desc_article_a"
+
+  articles[2] = {}
+  articles[2].title = "article_b"
+  articles[2].desc = "desc_article_b"
+
+  articles[3] = {}
+  articles[3].title = "article_c"
+  articles[3].desc = "desc_article_c"
+
+  self.articles = articles
+  return { render = "articles" }
 end)
 
 app:match("/person", function(self)
