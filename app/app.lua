b1d585402231b1c3babd0ef15c1524f0069dfb8a
mpolitzer <mpolitzer.c@gmail.com>
2016-06-29 22:12:32 -0300

    lisagem de artigos. adição e upload ainda quebrado

--- app/app.lua
+++ app/app.lua
@@ -1,7 +1,9 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+local Validate = require("lapis.validate")
 local Person = require("models.person")
 local Conference = require("models.conference")
+local Article = require("models.article")
 
 app:enable("etlua")
 app.layout = require("views.layout")
@@ -28,6 +30,14 @@ app:get("administrator", "/administrator", function(self)
     return { render = "administrator" }
 end)
 
+app:get("/articles", function(self)
+    self.articles    = Article:select() or {}
+    self.conferences = Conference:select() or {}
+
+    --return tostring(self.conferences[1].id)
+    return { render = "articles" }
+end)
+
 -- POST routes
 
 app:post("add_conference", "/conference", function(self)
@@ -40,6 +50,29 @@ app:post("add_conference", "/conference", function(self)
     })
     self.conferences = Conference:select()
     return { render = "conferences"}
+end)
+
+app:post("/article", function(self)
+    Validate.assert_valid(self.params, {
+        { "conference_id", exists = true },
+        { "file", is_file = true }
+    })
+
+    return self.params.conference
+    ------ solve this!
+    ----local conference = Conference:find(1)
+    ----local persons    = Person:find_all({1,2,3,4 ...})
+
+    --local article = Article:create({
+    --    title = self.params.title,
+    --    abstract = self.params.abstract,
+    --    conference_id = self.params.conference_id,
+    --    file_path = "static/articles/"..self.params.file,
+    --    download_count = 0
+    --})
+    --return "Created an article " .. self.params.title .. "\n" ..
+    --"in conference " .. conference.name .. "\n" ..
+    --"with file " .. self.params.file
 end)
 
 
---
+++ docs/diaries/marcelo-politzer.md
@@ -0,0 +1,10 @@
+# Diário de Hugo Grochau
+
+## 07/06/2016
+* criação de uma VM do VirtualBox configurada com o ambiente lapis+openresty
+
+## 20/06/2016
+* criação de um exemplo de como popular os templates no framework lapis
+
+## 29/06/2016
+* versão preliminar da inserção e listagem de artigos
